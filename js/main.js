7c29d2fa712792e76ee8f4fc478cddf9dd31acaf
= <=>
2017-02-28 22:25:41 +0800

    Signed-off-by: = <hsay1990@163.com>

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items:1,
+  	loop:true,
+  	autoplay:true,
+  	autoplayTimeout:1000,
+  	autoplayHoverPause:true,
+  	nav:true
+  });
+});
